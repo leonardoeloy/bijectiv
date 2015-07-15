424783ac187e3fef53178ddedbbaead49eb1d37e
Leonardo Eloy <leonardo.eloy@gmail.com>
2015-07-14 23:45:35 -0300

    fixed comments

--- bijectiv.go
+++ bijectiv.go
@@ -62,30 +62,3 @@ func reverse(s string) string {
 	}
 	return string(r)
 }
-
-/*
-public static String encode(int num)
-{
-StringBuilder sb = new StringBuilder();
-
-while ( num > 0 )
-{
-sb.append( ALPHABET.charAt( num % BASE ) );
-num /= BASE;
-}
-
-return sb.reverse().toString();
-}
-
-public static int decode(String str)
-{
-int num = 0;
-
-for ( int i = 0, len = str.length(); i < len; i++ )
-{
-num = num * BASE + ALPHABET.indexOf( str.charAt(i) );
-}
-
-return num;
-}
-*/
